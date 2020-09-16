42679ec553122a86130f2b029600d9803db15f85
kushkharb <43171979+kushkharb@users.noreply.github.com>
2020-09-16 08:06:51 +0530

    day1 class assignment

---
+++ day1/src/Demo.java
@@ -0,0 +1,11 @@
+
+/**
+ * 
+ * @author DELL
+ *
+ */
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Hello from java");
+	}
+}
---
+++ day1/src/Greeting.java
@@ -0,0 +1,10 @@
+
+public class Greeting {
+	public static void main(String[] args) {
+		String[] names = {"ram", "rahul", "kush","ram2","ram3","ram4","ram5","ram6","ram7","ram8"};	
+		int n = names.length;
+		for(int i = 0; i<n; i++){
+	          System.out.println("Hi "+ names[i]);
+	      }
+	}
+}
